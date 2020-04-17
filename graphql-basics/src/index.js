fcda7f9acc16b4794ac75b52f67ecf5abcc6a23a
dhruvsaxena1998 <saxenadhruv1927@gmail.com>
2020-04-18 01:34:04 +0530

    working with arrays

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -4,49 +4,15 @@ import { GraphQLServer } from "graphql-yoga";
 // Type Definitions (Schema)
 const typeDefs = `
   type Query {
-    me: User!
-    post: Post!
-    greeting(name: String): String!
-    add(a: Float!, b: Float!): Float!
+    grades: [Int!]!
   }
-  type User {
-    id: ID!
-    name: String!
-    email: String!
-    age: Int
-  }
-  type Post {
-    id: ID!
-    title: String!
-    body: String!
-    published: Boolean!
-  }
-
 `;
 
 // Resolvers (Set of functions)
 const resolvers = {
   Query: {
-    me () {
-      return {
-        id: 's0m3R4nD0mID',
-        name: 'Dhruv Saxena',
-        email: 'saxenadhruv1927@gmail.com'
-      }
-    },
-    post () {
-      return {
-        id: '4n07H3rR4nD0mID',
-        title: 'The Subtle Art of Not Giving a F*ck',
-        body: 'A Counterintuitive approach to living a good life',
-        published: true
-      }
-    },
-    greeting(parent, { name }, ctx, info) {
-      return !name ? 'Hello' : `Hello ${name}`
-    },
-    add (parent, {a, b}, ctx, info) {
-      return a + b
+    grades (parent, args, ctx, into) {
+      return [99, 70, 80, 93, 91]
     }
   },
 };
