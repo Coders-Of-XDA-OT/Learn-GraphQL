9f0528d47f44f1c2a00fdd1d26b2c4bbc87c7582
dhruvsaxena1998 <saxenadhruv1927@gmail.com>
2020-04-18 23:16:57 +0530

    relational data queries and resolvers

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -5,33 +5,39 @@ const users = [{
   id: '1',
   name: 'Dhruv Saxena',
   email: 'dhruv@example.com',
-  age: 22
+  age: 22,
+  posts: ['1', '2']
 }, {
   id: '2',
-  name: 'SomAeone',
-  email: 'someone@example.com'
+  name: 'Someone',
+  email: 'someone@example.com',
+  posts: ['3']
 }, {
   id: '3',
   name: 'Mike',
   email: 'mike@example.com',
-  age: 32
+  age: 32,
+  posts: []
 }]
 
 const posts = [{
   id: '1',
   title: 'HP 1',
   body: 'Harry Potter 1',
-  published: true
+  published: true,
+  author: '1'
 }, {
   id: '2',
   title: 'HP 2',
   body: 'Harry Potter 2',
-  published: true
+  published: true,
+  author: '1'
 }, {
   id: '3',
   title: 'Dark Lords',
   body: 'Dark Loards',
-  published: false
+  published: false,
+  author: '2'
 },]
 
 // Type Definitions (Schema)
@@ -45,14 +51,15 @@ const typeDefs = `
     name: String!
     email: String!
     age: Int
+    posts: [Post!]!
   }
   type Post {
     id: ID!
     title: String!
     body: String!
     published: Boolean!
+    author: User!
   }
-
 `;
 
 // Resolvers (Set of functions)
@@ -73,6 +80,20 @@ const resolvers = {
       })
     }
   },
+  User: {
+    posts (parent, args, ctx, info) {
+      return posts.filter((post) => {
+        return post.author === parent.id
+      })
+    }
+  },
+  Post: {
+    author (parent, args, ctx, info) {
+      return users.find((user) => {
+        return user.id === parent.author
+      })
+    }
+  }
 };
 
 // creating server
